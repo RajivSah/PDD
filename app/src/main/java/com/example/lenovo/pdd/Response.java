2b2dda9959639488a165fadcb5b0199a964c2046
katastreet <satyarth7a@hotmail.com>
2017-12-20 00:49:37 +0545

    added api communication

---
+++ app/src/main/java/com/example/lenovo/pdd/Response.java
@@ -0,0 +1,26 @@
+package com.example.lenovo.pdd;
+
+/**
+ * Created by satyarth on 19/12/17.
+ */
+
+public class Response {
+
+        String disease,filename;
+
+        public String getDisease() {
+            return disease;
+        }
+
+        public String getFilename() {
+            return filename;
+        }
+        public void setDisease(String disease) {
+            this.disease = disease;
+        }
+
+        public void setFilename(String filename) {
+            this.filename = filename;
+        }
+
+}
